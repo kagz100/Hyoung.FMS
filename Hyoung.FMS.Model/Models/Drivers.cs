d2fac9367da7a5e76a0cefd0978618e61d6d86e4
kevin kagiri <kevin.kagiri@hyoung.co.ke>
2020-02-05 17:57:24 +0300

    ef

--- Hyoung.FMS.Model/Models/Drivers.cs
+++ Hyoung.FMS.Model/Models/Drivers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hyoung.FMS.Model.Models
 {
@@ -11,9 +12,16 @@ namespace Hyoung.FMS.Model.Models
             Lightvehicleconsumption = new HashSet<Lightvehicleconsumption>();
         }
 
+        
         public int Id { get; set; }
+
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string EmpNo { get; set; }
         public double? PhoneNumber { get; set; }
         public double? DefaultAssignedVehicleId { get; set; }
--- Hyoung.FMS.WebServices/ReportServicebase.cs
+++ Hyoung.FMS.WebServices/ReportServicebase.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 using ReportingServiceReference1;
 
 namespace Hyoung.FMS.WebServices
@@ -22,10 +24,23 @@ namespace Hyoung.FMS.WebServices
   
         private string _sessionID;
 
+        private int _applicationID;
+
+        public int ApplicationID { get; set; }
         public  ReportServicebase ()
         {
 
             _sessionID = DirectoryServicesBase.GetSimpleService().SessionID;
+            _applicationID = ApplicationID;
+        }
+
+        public async Task<System.Xml.Linq.XNode> GetReports()
+        {
+            var results = await reportingSoap.GetReportsAsync(_sessionID, _applicationID);
+
+          
+
+            return results.Body.GetReportsResult;
         }
 
 
@@ -35,13 +50,23 @@ namespace Hyoung.FMS.WebServices
 
             var results = await reportingSoap.GenerateReportAsync(DirectoryServicesBase.GetSimpleService().SessionID, iReportID, startDate, endDate);
 
-
+            
             _handleID = int.Parse(results.Body.GenerateReportResult.FirstNode.ToString());
             return _handleID;
 
          }
 
+        public async Task<string> GetReportStatus(int iHandle)
+        {
+
+                         var results = await reportingSoap.GetReportStatusAsync(_sessionID, iHandle);
+            return results.Body.GetReportStatusResult.Value.ToString();
+        }
+
+
+#pragma warning disable IDE0060 // Remove unused parameter
         private void GetHandleID(System.Xml.XmlElement results)
+#pragma warning restore IDE0060 // Remove unused parameter
         {
             throw new NotImplementedException();
         }

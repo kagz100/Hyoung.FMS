47bc20330e58452513c7b193f1c0361266c3b185
kevin kagiri <kevin.kagiri@hyoung.co.ke>
2023-03-28 17:09:13 +0300

    123

--- FMS.Infrastructure/Webservice/GPSGateDirectoryWebservice.cs
+++ FMS.Infrastructure/Webservice/GPSGateDirectoryWebservice.cs
@@ -1,4 +1,5 @@
-﻿using FMS.Domain.Entities.Auth;
+﻿using FMS.Domain.Entities;
+using FMS.Domain.Entities.Auth;
 using FMS.Domain.Entities.Common.Interface;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,38 @@ namespace FMS.Infrastructure.Webservice
         static readonly DirectoryServiceReference1.DirectorySoapClient _DirectorySoapClient = new DirectoryServiceReference1.DirectorySoapClient(DirectoryServiceReference1.DirectorySoapClient.EndpointConfiguration.DirectorySoap12);
         static readonly ReportingServiceReference.ReportingSoapClient _ReportSoapClient = new ReportingServiceReference.ReportingSoapClient(ReportingServiceReference.ReportingSoapClient.EndpointConfiguration.ReportingSoap12);
 
+  
+       
+
+        public async Task<List<Vehicleconsumption>> GetFuelConsumptionReport(GPSGateConections conn,int FuelConsumptionReportID, DateTime from, DateTime to)
+        {
+            //Remember that 208 is hardcorded ReportID for Fuel Consumption Report
+            //If you need to changed it, you need to get it in GPSGate using postman
+            
+            
+            var results = await _ReportSoapClient.GenerateReportAsync(conn.SessionID, 208, from, to);
+            try
+            {
+               //chcke for errors 
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+   
+            return results.ToString();
+
+        }
+
+
+            public void CheckError(XmlNode element)
+        {
+            if (element != null && element.FirstChild != null && element.FirstChild.Name == "exception")
+            {
+                throw new Exception(element.SelectSingleNode("//exception/message").InnerText);
+            }
+        }
+
         public async Task<string> LoginAsyn(GPSGateConections conn)
         {
             var results = await _DirectorySoapClient.LoginAsync(conn.GPSGateUser.UserName, conn.GPSGateUser.Password, conn.ApplicationID);
@@ -21,7 +54,7 @@ namespace FMS.Infrastructure.Webservice
             {
                 CheckError(results);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return e.Message;
             }
@@ -36,43 +69,6 @@ namespace FMS.Infrastructure.Webservice
 
             return results.Name;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="conn"></param>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        /// <returns></returns>
-
-        public async Task<string> GetFuelConsumptionReport(GPSGateConections conn,int FuelConsumptionReportID, DateTime from, DateTime to)
-        {
-            //Get FuelconsumptionReport ID from Database
-
-
-            
-            var results = await _ReportSoapClient.GenerateReportAsync(conn.SessionID, 208, from, to);
-            try
-            {
-               //chcke for errors
-            }
-            catch (Exception e)
-            {
-                return e.Message;
-            }
-            
-            return results.ToString();
-
-        }
-
-
-            public void CheckError(XmlNode element)
-        {
-            if (element != null && element.FirstChild != null && element.FirstChild.Name == "exception")
-            {
-                throw new Exception(element.SelectSingleNode("//exception/message").InnerText);
-            }
-        }
-
         public Task<string> GetFuelconsumptionReport(GPSGateConections conn, int FuelConsumptionReportID, DateTime from, DateTime to)
         {
             throw new NotImplementedException();
